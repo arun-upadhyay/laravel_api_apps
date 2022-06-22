274dff848e01e09186784fe16aaa89214d019230
Arun Upadhyay <arun.upadhyay@csquaredsystems.com>
2022-06-22 17:38:40 -0400

    Logout response added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -74,7 +74,7 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->logout();
-        return response()->json(['message' => 'User successfully signed out']);
+        return response()->json(['message' => 'User successfully signed out'], 200);
     }
 
     /**

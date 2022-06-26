4f2e174f5ecdeea0a1f23b03eb5d1f6d543d65d5
Arun Upadhyay <arunnnet@gmail.com>
2022-06-26 18:45:22 -0400

    Managed existing migration file

--- database/migrations/2022_06_22_103331_create_posts_table.php
+++ database/migrations/2022_06_22_103331_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('title');
-            $table->string('description');
+            $table->json('body')->nullable();
             $table->timestamps();
         });
     }
